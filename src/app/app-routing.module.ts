a63e4cf6b76b37ff70adef76e16dba0bdb964ed8
jaovg5 <jvgsantos05@gmail.com>
2023-10-08 00:43:22 -0300

    Meu primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MiddleComponent } from './component/middle/middle.component';
+import { LoginComponent } from './component/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MiddleComponent},
+  {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
